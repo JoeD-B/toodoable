023abbb14b4737ce1936a1d04fed816c399aff6a
Joe Downes-Berry <j.d.berry26@gmail.com>
2021-04-01 02:27:26 -0400

    added views and routes for users/sessions

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   resources :todos
 
+  get '/signup', to: "users#new", as: "signup"
+  post '/signup', to: "users#create"
+  get '/login', to: "sessions#new", as: "login"
+  post '/login', to: "sessions#create"
+  post '/logout', to: "sessions#destroy"
 end
