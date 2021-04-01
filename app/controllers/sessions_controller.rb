7c50d0b12831222bc8a285c808e85f5576cda207
Joe Downes-Berry <j.d.berry26@gmail.com>
2021-04-01 02:15:17 -0400

    Created and added code for SessionController and added code for UserController

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+    def new
+      # @user = User.new
+    end
+  
+    def create
+      @user = User.find_by(username: params[:user][:username])
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to user_items_path(@user)
+      elsif @user
+        @errors = ["Invalid Password"]
+        render :new
+      else
+        @errors = ["Invalid Username"]
+        render :new
+      end
+    end
+  
+    def destroy
+      session.clear
+      redirect_to '/signup'
+    end
+  
+  
+end
